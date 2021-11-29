e891f905f7313dc1037e5cd8b1cff6ebbafb2f93
exxsid <cortezleoanthony5@gmail.com>
2021-11-29 11:03:35 +0800

    added GUI component fields

--- src/TempConverterGUI.java
+++ src/TempConverterGUI.java
@@ -1,3 +1,24 @@
+import javax.swing.*;
+import javax.swing.plaf.BorderUIResource;
+import java.awt.*;
+
 public class TempConverterGUI {
-    
+
+    // three panels for containing the first and second temperature
+    // option and convert button
+    private JPanel firstPanel = new JPanel();
+    private JPanel secPanel = new JPanel();
+    private JPanel convPanel = new JPanel();
+
+    // display and get input from the user
+    private JTextArea firstTxtAr = new JTextArea();
+    private JTextArea secTxtAr = new JTextArea();
+
+    // display the options for temperature conversion
+    private final String[] choices = {"Celsius", "Fahrenheit", "Kelvin"};
+    private JComboBox<String> firstChoice = new JComboBox<>(choices);
+    private JComboBox<String> secChoice = new JComboBox<>(choices);
+
+
+
 }
