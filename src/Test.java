e26eefb47751a3ab70423021552dc2bd64d8e873
exxsid <cortezleoanthony5@gmail.com>
2021-11-29 11:02:33 +0800

    Added Test.java class

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Press any key");
+
+        int x = System.in.read();
+        System.out.println(x);
+    }
+}
